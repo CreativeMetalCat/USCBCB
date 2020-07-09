2e50223164082ab1fc21969bb65f73273bb01ea2
CreativeMetalCat <catguy228@gmail.com>
2020-07-09 21:51:16 +1000

    added raw .ogg files

--- Content/LuaCode/Player.lua
+++ Content/LuaCode/Player.lua
@@ -3,4 +3,11 @@ local Player = {}
 function Player:BeginPlay()
 end
 
+function Player:OnFootstep()
+end
+
+function Player:GetFootstepSoundFilename()
+    return nil;--currenty no files are avalable
+end
+
 return Player;
